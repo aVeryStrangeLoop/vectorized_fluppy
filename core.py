d144437c9fd9703af9dcdb2c6acf938ced788fec
Bhaskar <kmwtbhaskar@gmail.com>
2020-05-23 22:10:21 +0530

    Changed fluppy to core and added a __main__ descriptor, also added output files:

--- core.py
+++ core.py
@@ -1,90 +1,64 @@
-
-
 import numpy as np
+import time
 import os
 import sys
 import matplotlib.pyplot as plt
 from scipy.signal import convolve
 
-if len(sys.argv)!=15:
-    print("usage: flup.py -n <Grid size> -u <Utility Function> -t <number of tasks> -c <compCap> -i <insight> -l <leakage> -o <output_file>")
-    sys.exit
-
-# Run parameters 
-n = 3 # Grid size (no. of agents = 3x3)
-nTasks = 10 # No. of tasks in the environment
-compCap = 10 # Computational capacity (genome size)
-insight = 0.2 # Insight for certain utility functions
-outfile = open("out.txt","w+") # Output file
-exp_eff = 0.10 # Export efficiency
-
-outfile.write("gen,best_sw\n")
-
-# GA parameters
-pop_size = 100
-mutation_prob = 0.1
-crossover_prob = 0.75
-number_of_gen = 1000
-
-# Task to info parameters
-alpha = 3.
-beta = 5.
 
 def init_Pop():
-  population = np.random.randint(nTasks, size=(pop_size,n,n,compCap))
-  return population
+    population = np.random.randint(nTasks, size=(pop_size,n,n,compCap))
+    return population
 
 def uf(res):
-  return np.sum(res)
+    return np.sum(res)
 
 def grid_uf(res_grid):
-  ufs = np.zeros((n,n))
-  for i in range(res_grid.shape[0]):
-    for j in range(res_grid.shape[1]):
-      ufs[i,j] = uf(res_grid[i,j])
-  return ufs
+    ufs = np.zeros((n,n))
+    for i in range(res_grid.shape[0]):
+        for j in range(res_grid.shape[1]):
+            ufs[i,j] = uf(res_grid[i,j])
+    return ufs
 
 # This function returns the resource and donation vectors after processing the genome (NOTE: Zero task has no resource acquisition)
 def genome_to_res_and_donate(genome):
-  res = np.zeros(nTasks)
-  genome_pos = genome[genome>0]
-  res[genome_pos] = genome_pos*alpha+beta
-  donate = np.zeros(nTasks)
-  genome_neg = abs(genome[genome<0])
-  donate[genome_neg] += res[genome_neg]*exp_eff
-  res[genome_neg] -= res[genome_neg]*exp_eff
-  return res,donate/8.
+    res = np.zeros(nTasks)
+    genome_pos = genome[genome>0]
+    res[genome_pos] = genome_pos*alpha+beta
+    donate = np.zeros(nTasks)
+    genome_neg = abs(genome[genome<0])
+    donate[genome_neg] += res[genome_neg]*exp_eff
+    res[genome_neg] -= res[genome_neg]*exp_eff
+    return res,donate/8.
 
 def grid_gtrad(grid):
-  res = np.zeros((n,n,nTasks))
-  donate = np.zeros((n,n,nTasks))
-  for i in range(grid.shape[0]):
-    for j in range(grid.shape[1]):
-      res[i,j],donate[i,j] = genome_to_res_and_donate(grid[i,j])
-  return res,donate
+    res = np.zeros((n,n,nTasks))
+    donate = np.zeros((n,n,nTasks))
+    for i in range(grid.shape[0]):
+        for j in range(grid.shape[1]):
+            res[i,j],donate[i,j] = genome_to_res_and_donate(grid[i,j])
+    return res,donate
 
 def grid_sw(grid):
-  # Get resource vector for whole grid
-  grid_res,grid_donate = grid_gtrad(grid)
-  #print grid_donate
-  grid_donate = np.pad(grid_donate,((1,1),(1,1),(0,0)),'wrap')
-  # grid_res_donated contains the unpacked result of grid_donate too be added to each agent's resource reservoir
-  kernel = np.array([[1.,1.,1.],[1.,0.,1.],[1.,1.,1.]])
-  kernel = kernel[:,:,None] 
-  grid_donated = convolve(grid_donate,kernel,mode='valid')
-  #print grid_donated 
-  grid_res = grid_res + grid_donated  
-  return np.sum(grid_uf(grid_res))
+    # Get resource vector for whole grid
+    grid_res,grid_donate = grid_gtrad(grid)
+    #print grid_donate
+    grid_donate = np.pad(grid_donate,((1,1),(1,1),(0,0)),'wrap')
+    # grid_res_donated contains the unpacked result of grid_donate too be added to each agent's resource reservoir
+    kernel = np.array([[1.,1.,1.],[1.,0.,1.],[1.,1.,1.]])
+    kernel = kernel[:,:,None] 
+    grid_donated = convolve(grid_donate,kernel,mode='valid')
+    #print grid_donated 
+    grid_res = grid_res + grid_donated  
+    return np.sum(grid_uf(grid_res))
 
 def social_welfare(pop):
-  sw_ar = np.zeros(pop.shape[0])
-  for i in range(pop.shape[0]):
-    sw_ar[i] = grid_sw(pop[i])
-  return sw_ar
+    sw_ar = np.zeros(pop.shape[0])
+    for i in range(pop.shape[0]):
+        sw_ar[i] = grid_sw(pop[i])
+    return sw_ar
 
-# Genetic Algorithm
-def crossover(parent1, parent2):
-    
+def crossover(parent1, parent2):    
     rand1 = np.random.randint(n)
     rand2 = np.random.randint(n)
     rand3 = np.random.randint(compCap)
@@ -121,7 +95,6 @@ def crossover(parent1, parent2):
 
     temp_2 = np.asarray([temp_2])
 
-#    np.vstack(list(temp))
     grid1 = np.vstack((parent1[:rand1],temp,parent2[rand1+1:]))
     
     grid2 = np.vstack((parent2[:rand1],temp_2,parent1[rand1+1:]))
@@ -202,34 +175,50 @@ def genetic_algorithm(population):
             
     return np.asarray(newpop)
 
-import time
-
-start_time = time.time()
-
-a = init_Pop()
-
-b = genetic_algorithm(a)
-
-best = []
-
-for i in range(number_of_gen):
-    b = genetic_algorithm(b)
-    soc = np.asarray(social_welfare(b))
-    best.append(soc.max())
-    outfile.write("%d,%f\n" % (i,best[-1]))
-    print("%d,%f" % (i,best[-1]))
+def DoRun(n, choice_of_u, nTasks, compCap, insight, exp_eff, pop_size, mutation_prob, crossover_prob, number_of_gen, alpha, beta,save_every):
+    a = init_Pop()
+    b = genetic_algorithm(a)
+    best_sw = []
+    best_grid = []
     
-outfile.close()    
-    
-fitness_arr = np.asarray(social_welfare(b)) #Returns numpy array of fitness
+    # Create output folder and file
+    if not os.path.exists('results'):
+        os.makedirs('results')
+    outfile = open("results/best_sw.csv","w+")
+    outfile.write("generation,best_sw\n")
 
-inds = fitness_arr.argsort()[::-1] #Gives indices from max to min
+    for i in range(number_of_gen):
+        #print i
+        b = genetic_algorithm(b)
+        fitness_arr = np.asarray(social_welfare(b)) #Returns numpy array of fitness
+        inds = fitness_arr.argsort()[::-1] #Gives indices from max to min
+        sortedPop = b[inds] #Sorts population based on fitness from highest to lowest
+        outfile.write("%d,%f\n" % (i,fitness_arr.max()))
+        if i%save_every==0:
+            #print sortedPop[0]
+            # Saves array in numpy readable format
+            np.save("results/best_grid_"+str(i),sortedPop[0])
+        #best_sw.append(fitness_arr.max())
+        #best_grid.append(sortedPop[0])
+    #return best_grid, best_sw
 
-sortedPop = b[inds] #Sorts population based on fitness from highest to lowest
-print(b[0])
 
-print("--- %s seconds ---" % (time.time() - start_time))
+if __name__=="__main__":
+    n = 3
+    choice_of_u = 2
+    nTasks = 10
+    compCap = 10
+    insight = 0.2
+    exp_eff = 0.10
 
+    pop_size = 100
+    mutation_prob = 0.1
+    crossover_prob = 0.75
+    number_of_gen = 1000
 
-#plt.plot(best)
+    alpha = 3.
+    beta = 5.
 
+    save_every = 100
+
+    DoRun(n,choice_of_u,nTasks,compCap,insight,exp_eff,pop_size,mutation_prob,crossover_prob,number_of_gen,alpha,beta,save_every)
